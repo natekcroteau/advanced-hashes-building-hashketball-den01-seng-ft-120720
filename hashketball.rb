dad0c0aba977874706811add59bdc3fe2f98e39e
Nate Croteau <natekcroteau@gmail.com>
2020-11-27 16:05:08 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -4,8 +4,13 @@ def game_hash
   nds = {}
   nds[:home] = {}
   nds[:away] = {}
-  
-  #binding.pry 
+  nds[:home][:team_name] = ""
+  nds[:home][:colors] = []
+  nds[:home][:players] = {}
+  nds[:away][:team_name] = ""
+  nds[:away][:colors] = []
+  nds[:away][:players] = {}
+  #binding.pry  
   
   nds 
 end
