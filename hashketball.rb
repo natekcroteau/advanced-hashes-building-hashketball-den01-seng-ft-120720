5e22a8081487a2c4f406e5494b44a30fb497215e
Nate Croteau <natekcroteau@gmail.com>
2020-11-27 16:08:36 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -4,13 +4,26 @@ def game_hash
   nds = {}
   nds[:home] = {}
   nds[:away] = {}
+  
   nds[:home][:team_name] = ""
   nds[:home][:colors] = []
   nds[:home][:players] = {}
+  
   nds[:away][:team_name] = ""
   nds[:away][:colors] = []
   nds[:away][:players] = {}
-  nds[:home][:players][:] = 
+  
+  nds[:home][:players][:player_name] = ""
+  nds[:home][:players][:number] = ""
+  nds[:home][:players][:shoe] = ""
+  nds[:home][:players][:points] = ""
+  nds[:home][:players][:rebounds] = ""
+  nds[:home][:players][:assists] = ""
+  nds[:home][:players][:steals] = ""
+  nds[:home][:players][:blocks] = ""
+  nds[:home][:players][:slam_dunks] = ""
+  
+  
   #binding.pry  
   
   nds 
