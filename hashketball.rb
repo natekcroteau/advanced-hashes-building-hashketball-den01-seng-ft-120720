77cd3df2ba7a235e63e2fc4beeb4acab1d985504
Nate Croteau <natekcroteau@gmail.com>
2020-11-27 16:34:10 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -10,64 +10,65 @@ def game_hash
   nds[:home][:colors] = []
   nds[:home][:players] = []
   
+  
   nds[:away][:team_name] = "Charlotte Hornets"
   nds[:away][:colors] = []
   nds[:away][:players] = []
   
+  nds[:home][:players][0] = {}
+  nds[:home][:players][0][:player_name] = "Alan Anderson"
+  nds[:home][:players][0][:number] = 0 
+  nds[:home][:players][0][:shoe] = 16
+  nds[:home][:players][0][:points] = 22
+  nds[:home][:players][0][:rebounds] = 12
+  nds[:home][:players][0][:assists] = 12
+  nds[:home][:players][0][:steals] = 3
+  nds[:home][:players][0][:blocks] = 1 
+  nds[:home][:players][0][:slam_dunks] = 1 
   
-  nds[:home][:players][:player_name] = "Alan Anderson"
-  nds[:home][:players][:number] = 0 
-  nds[:home][:players][:shoe] = 16
-  nds[:home][:players][:points] = 22
-  nds[:home][:players][:rebounds] = 12
-  nds[:home][:players][:assists] = 12
-  nds[:home][:players][:steals] = 3
-  nds[:home][:players][:blocks] = 1 
-  nds[:home][:players][:slam_dunks] = 1 
+  nds[:home][:players][1] = {}
+  nds[:home][:players][1][:player_name] = "Reggie Evans"
+  nds[:home][:players][1][:number] = 30 
+  nds[:home][:players][1][:shoe] = 14
+  nds[:home][:players][1][:points] = 12
+  nds[:home][:players][1][:rebounds] = 12
+  nds[:home][:players][1][:assists] = 12
+  nds[:home][:players][1][:steals] = 12
+  nds[:home][:players][1][:blocks] = 12 
+  nds[:home][:players][1][:slam_dunks] = 7 
   
+  nds[:home][:players][2] = {}
+  nds[:home][:players][2][:player_name] = "Brook Lopez"
+  nds[:home][:players][2][:number] = 11 
+  nds[:home][:players][2][:shoe] = 17
+  nds[:home][:players][2][:points] = 17
+  nds[:home][:players][2][:rebounds] = 19
+  nds[:home][:players][2][:assists] = 10
+  nds[:home][:players][2][:steals] = 3
+  nds[:home][:players][2][:blocks] = 1
+  nds[:home][:players][2][:slam_dunks] = 15 
   
-  nds[:home][:players][:player_name] = "Reggie Evans"
-  nds[:home][:players][:number] = 30 
-  nds[:home][:players][:shoe] = 14
-  nds[:home][:players][:points] = 12
-  nds[:home][:players][:rebounds] = 12
-  nds[:home][:players][:assists] = 12
-  nds[:home][:players][:steals] = 12
-  nds[:home][:players][:blocks] = 12 
-  nds[:home][:players][:slam_dunks] = 7 
+  nds[:home][:players][3] = {}
+  nds[:home][:players][3][:player_name] = "Mason Plumlee"
+  nds[:home][:players][3][:number] = 1 
+  nds[:home][:players][3][:shoe] = 19
+  nds[:home][:players][3][:points] = 26
+  nds[:home][:players][3][:rebounds] = 11
+  nds[:home][:players][3][:assists] = 6 
+  nds[:home][:players][3][:steals] = 3
+  nds[:home][:players][3][:blocks] = 8
+  nds[:home][:players][3][:slam_dunks] = 5
   
-  
-  nds[:home][:players][:player_name] = "Brook Lopez"
-  nds[:home][:players][:number] = 11 
-  nds[:home][:players][:shoe] = 17
-  nds[:home][:players][:points] = 17
-  nds[:home][:players][:rebounds] = 19
-  nds[:home][:players][:assists] = 10
-  nds[:home][:players][:steals] = 3
-  nds[:home][:players][:blocks] = 1
-  nds[:home][:players][:slam_dunks] = 15 
-  
-  
-  nds[:home][:players][:player_name] = "Mason Plumlee"
-  nds[:home][:players][:number] = 1 
-  nds[:home][:players][:shoe] = 19
-  nds[:home][:players][:points] = 26
-  nds[:home][:players][:rebounds] = 11
-  nds[:home][:players][:assists] = 6 
-  nds[:home][:players][:steals] = 3
-  nds[:home][:players][:blocks] = 8
-  nds[:home][:players][:slam_dunks] = 5
-  
-  
-  nds[:home][:players][:player_name] = "Jason Terry"
-  nds[:home][:players][:number] = 31 
-  nds[:home][:players][:shoe] = 15
-  nds[:home][:players][:points] = 19
-  nds[:home][:players][:rebounds] = 2
-  nds[:home][:players][:assists] = 2
-  nds[:home][:players][:steals] = 4
-  nds[:home][:players][:blocks] = 11
-  nds[:home][:players][:slam_dunks] = 1
+  nds[:home][:players][4] = {}
+  nds[:home][:players][4][:player_name] = "Jason Terry"
+  nds[:home][:players][4][:number] = 31 
+  nds[:home][:players][4][:shoe] = 15
+  nds[:home][:players][4][:points] = 19
+  nds[:home][:players][4][:rebounds] = 2
+  nds[:home][:players][4][:assists] = 2
+  nds[:home][:players][4][:steals] = 4
+  nds[:home][:players][4][:blocks] = 11
+  nds[:home][:players][4][:slam_dunks] = 1
   nds 
   binding.pry  
   
