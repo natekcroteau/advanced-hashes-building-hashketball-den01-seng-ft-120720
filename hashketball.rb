4a05b2ca46de7f7b0da62b928ae67c185a30e4db
Nate Croteau <natekcroteau@gmail.com>
2020-11-27 16:15:53 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -34,6 +34,36 @@ def game_hash
   nds[:home][:players][:blocks] = 12 
   nds[:home][:players][:slam_dunks] = 7 
   
+  nds[:home][:players][:player_name] = "Brook Lopez"
+  nds[:home][:players][:number] = 11 
+  nds[:home][:players][:shoe] = 17
+  nds[:home][:players][:points] = 17
+  nds[:home][:players][:rebounds] = 19
+  nds[:home][:players][:assists] = 10
+  nds[:home][:players][:steals] = 3
+  nds[:home][:players][:blocks] = 1
+  nds[:home][:players][:slam_dunks] = 15 
+  
+  nds[:home][:players][:player_name] = "Mason Plumlee"
+  nds[:home][:players][:number] = 1 
+  nds[:home][:players][:shoe] = 19
+  nds[:home][:players][:points] = 26
+  nds[:home][:players][:rebounds] = 11
+  nds[:home][:players][:assists] = 6 
+  nds[:home][:players][:steals] = 3
+  nds[:home][:players][:blocks] = 8
+  nds[:home][:players][:slam_dunks] = 5
+  
+  nds[:home][:players][:player_name] = "Jason Terry"
+  nds[:home][:players][:number] = 31 
+  nds[:home][:players][:shoe] = 15
+  nds[:home][:players][:points] = 19
+  nds[:home][:players][:rebounds] = 2
+  nds[:home][:players][:assists] = 2
+  nds[:home][:players][:steals] = 4
+  nds[:home][:players][:blocks] = 11
+  nds[:home][:players][:slam_dunks] = 1
+  
   #binding.pry  
   
   nds 
